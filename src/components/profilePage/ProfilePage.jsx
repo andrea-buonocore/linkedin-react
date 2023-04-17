0ecb39f11cc6210d4af11a1f8a6c121c92e0d80e
andrea-buonocore <andreabuonocore00@gmail.com>
2023-04-17 15:00:47 +0200

    Preparato lo schema, adesso lavoriamo sui singoli componenti

---
+++ src/components/profilePage/ProfilePage.jsx
@@ -0,0 +1,29 @@
+import { Col, Container, Row } from "react-bootstrap";
+import Footer from "../Footer";
+
+const ProfilePage = () => {
+    return (
+        <Container id="profilePageContainer">
+            <Row>
+                {/* COLONNA SX */}
+                <Col xs={12} md={6} lg={8}>
+                    <Row xs={1}>
+                        <Col>Colonna sx</Col>
+                    </Row>
+                </Col>
+                {/* FINE COLONNA SX */}
+
+                {/* COLONNA DX */}
+                <Col xs={12} md={6} lg={4}>
+                    <Row xs={1}>
+                        <Col>Colonna dx</Col>
+                    </Row>
+                </Col>
+                {/* FINE COLONNA DX */}
+            </Row>
+            <Footer/>
+        </Container>
+    )
+}
+
+export default ProfilePage;
