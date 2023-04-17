05da24e06301e552eb825f9d276d189c2d6097d9
gretel-hans <adjeihansel@icloud.com>
2023-04-18 01:45:54 +0200

    .

--- src/components/profilePage/profileSubComponents/ProfileBigCard.jsx
+++ src/components/profilePage/profileSubComponents/ProfileBigCard.jsx
@@ -1,3 +1,5 @@
+import { Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 const ProfileBigCard = () => {
   return (
     <div className="ProfileBigCardContainer">
@@ -13,8 +15,8 @@ const ProfileBigCard = () => {
           alt="profilepic"
           src="https://media.licdn.com/dms/image/D4E35AQEI-SA5CGOldg/profile-framedphoto-shrink_200_200/0/1680040710302?e=1682326800&v=beta&t=pQUm4JEVPJ3p9SAmPWX9ZS6VEEV4vpiQ3ePd64Bcia4"
         />
-        
-      </div><div className="pencil-right">ciao</div>
+      </div>
+      <div className="pencil-right">ciao</div>
       <div className="ProfileCoverPencil">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -29,8 +31,58 @@ const ProfileBigCard = () => {
           <path d="M14.13 1.86a3 3 0 00-4.17 0l-7 7L1 15l6.19-2 6.94-7a3 3 0 000-4.16zm-8.36 9.71l-1.35-1.34L9.64 5 11 6.35z"></path>
         </svg>
       </div>
-      
-      <div className="profileBigCardTextContainer">Hansel Sarpong</div>
+
+      <div className="profileBigCardTextContainer">
+        <h5 className="mb-0">Hansel Sarpong Adjei </h5>
+        <p className="mb-2">
+          <small>Junior Full-Stack Web Developer </small>
+        </p>
+        <p className="mb-2 text-secondary">
+          <small>EPICODE </small>
+        </p>
+        <p className="text-secondary mb-2">
+          <small>Reggio nell'Emilia, Emilia Romagna, Italia • </small>
+          <span className="text-primary">Informazioni di contatto </span>
+        </p>
+        <span className="text-primary">78 collegamenti </span>
+        <div className=" mt-2 ProfilePicButtons">
+          <Button className="me-2" id="buttonProfileBigCard">
+            <span>Disponibile per </span>
+          </Button>
+          <Button
+            className="me-2"
+            id="buttonProfileBigCard"
+            variant="outline-primary"
+          >
+            <span className="text-primary">Aggiungi sezione del profilo </span>
+          </Button>
+          <Button id="buttonProfileBigCard" variant="outline-secondary">
+            <span className="text-secondary">Altro </span>
+          </Button>
+        </div>
+        <div className="carousel container mt-3">
+          <div className="carouselCard1 d-flex me-3">
+            <div className="carouselTextContainer">
+            <p className='mb-0'>Disponibilie a lavorare</p>
+            <p className='mb-0'>Ruoli di Sviluppatore Html5, Sviluppatore </p>
+            <p className='mb-0 text-primary'>Mostra dettagli</p>
+            </div>
+            <div>
+            <i class="bi bi-pencil"></i>
+            </div>
+          </div>
+          <div className="carouselCard1 d-flex">
+            <div className="carouselTextContainer">
+            <p className='mb-0'>Disponibilie a lavorare</p>
+            <p className='mb-0'>Ruoli di Sviluppatore Html5, Sviluppatore </p>
+            <p className='mb-0 text-primary'>Mostra dettagli</p>
+            </div>
+            <div>
+            <i class="bi bi-pencil"></i>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
