77177e93d0eeb64199e03b79e11cd98f511a169f
andrea-buonocore <andreabuonocore00@gmail.com>
2023-04-18 10:33:30 +0200

    ok competenze è responsive

--- src/components/profilePage/Competenze.jsx
+++ src/components/profilePage/Competenze.jsx
@@ -8,11 +8,12 @@ const Competenze = () => {
                 <div className="d-flex justify-content-between mb-2">
                     <Card.Title>Competenze</Card.Title>
                     <div>
-                        <Button variant="outline-primary">Quiz valutazione competenze</Button>
+                        <Button className="d-none d-lg-inline" variant="outline-primary">Quiz valutazione competenze</Button>
                         <i className="bi bi-plus-lg px-3"></i>
                         <i className="bi bi-pencil"></i>                        
                     </div>
                 </div>
+                <Button className="d-block d-lg-none mb-3" variant="outline-primary">Quiz valutazione competenze</Button>
                 {/* COMPETENZA 1 */}
                 <p className="fw-bold m-0">Competenza</p>
                 <hr/>
