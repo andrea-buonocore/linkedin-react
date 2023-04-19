b5cfed727c00daad1ad612164fda621b567cb733
gretel-hans <adjeihansel@icloud.com>
2023-04-20 00:02:43 +0200

    .

--- src/components/homePage/ColonnaCentrale/creazionePost.jsx
+++ src/components/homePage/ColonnaCentrale/creazionePost.jsx
@@ -1,13 +1,40 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
+const token ='Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDQwMmEwNWQ5MTJlOTAwMTRjMjNmYTMiLCJpYXQiOjE2ODE5MjY2NjIsImV4cCI6MTY4MzEzNjI2Mn0.s5TVsYd1HXaVaF0WJGJgDu1B2fdFiL0a6OxN5U3_aFk'
 
 const CreazionePost = () => {
   const [show, setShow] = useState(false);
+  const [comment,setComment]=useState(null)
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+let commentToSend={text:''}
+
+  const sendComment=()=>{
+    return(
+    fetch('https://striveschool-api.herokuapp.com/api/posts',{
+        method:'POST',
+        headers:{
+            'Content-Type':'aplication/json',
+            "Authorization": token
+        },
+        body: JSON.stringify({commentToSend})
+    })
+    .then(response=>{
+        if(response.ok){
+            return(
+                response.json(),
+                alert('Your comment has been posted!')
+                ) 
+        }else{
+            alert('ERROR your comment hasn\'t been posted!')
+        }
+    })
+    .catch(error=>console.log('ERROR',error))
+    ) 
+  }
+
   return (
     <div className="card mb-3">
       <div className="d-flex flex-column ">
@@ -103,13 +130,44 @@ const CreazionePost = () => {
                   rows={3}
                   placeholder="Di cosa vorresti parlare?"
                   className="textPostArea"
+                  value={comment}
+                  onChange={(e)=>{
+                    setComment(e.target.value)
+                  }}
                 />
               </Form.Group>
+
               <Form.Group>
-                <div>
+                <button className="buttonPost" onClick={(e)=>e.preventDefault()}>
+                                    <li-icon
+                  aria-hidden="true"
+                  type="emoji-face-icon"
+                  class="artdeco-button__icon"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    data-supported-dps="24x24"
+                    fill="currentColor"
+                    class="mercado-match gray"
+                    width="24"
+                    height="24"
+                    focusable="false"
+                  >
+                    <path d="M8 10.5A1.5 1.5 0 119.5 12 1.5 1.5 0 018 10.5zm6.5 1.5a1.5 1.5 0 10-1.5-1.5 1.5 1.5 0 001.5 1.5zm7.5 0A10 10 0 1112 2a10 10 0 0110 10zm-2 0a8 8 0 10-8 8 8 8 0 008-8zm-8 4a6 6 0 01-4.24-1.76l-.71.76a7 7 0 009.89 0l-.71-.71A6 6 0 0112 16z"></path>
+                  </svg>
+                </li-icon>
+                </button>
+
+              </Form.Group>
+            </Form>
+
+            <div className="mt-4 d-flex justify-content-between align-items-center">
+              <div className="firstContainerPostIcons">
+                <button className="buttonPost">
                   <li-icon
                     aria-hidden="true"
-                    type="emoji-face-icon"
+                    type="image"
                     class="artdeco-button__icon"
                   >
                     <svg
@@ -117,28 +175,142 @@ const CreazionePost = () => {
                       viewBox="0 0 24 24"
                       data-supported-dps="24x24"
                       fill="currentColor"
-                      class="mercado-match"
+                      class="mercado-match gray"
                       width="24"
                       height="24"
                       focusable="false"
                     >
-                      <path d="M8 10.5A1.5 1.5 0 119.5 12 1.5 1.5 0 018 10.5zm6.5 1.5a1.5 1.5 0 10-1.5-1.5 1.5 1.5 0 001.5 1.5zm7.5 0A10 10 0 1112 2a10 10 0 0110 10zm-2 0a8 8 0 10-8 8 8 8 0 008-8zm-8 4a6 6 0 01-4.24-1.76l-.71.76a7 7 0 009.89 0l-.71-.71A6 6 0 0112 16z"></path>
+                      <path d="M19 4H5a3 3 0 00-3 3v10a3 3 0 003 3h14a3 3 0 003-3V7a3 3 0 00-3-3zm1 13a1 1 0 01-.29.71L16 14l-2 2-6-6-4 4V7a1 1 0 011-1h14a1 1 0 011 1zm-2-7a2 2 0 11-2-2 2 2 0 012 2z"></path>
                     </svg>
                   </li-icon>
-                </div>
-              </Form.Group>
-            </Form>
+                </button>
+
+                <button className="buttonPost">
+                  <li-icon
+                    aria-hidden="true"
+                    type="video"
+                    class="artdeco-button__icon"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      data-supported-dps="24x24"
+                      fill="currentColor"
+                      class="mercado-match gray"
+                      width="24"
+                      height="24"
+                      focusable="false"
+                    >
+                      <path d="M19 4H5a3 3 0 00-3 3v10a3 3 0 003 3h14a3 3 0 003-3V7a3 3 0 00-3-3zm-9 12V8l6 4z"></path>
+                    </svg>
+                  </li-icon>
+                </button>
+
+                <button className="buttonPost">
+                  <li-icon
+                    aria-hidden="true"
+                    type="sticky-note"
+                    class="artdeco-button__icon"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      data-supported-dps="24x24"
+                      fill="currentColor"
+                      class="mercado-match gray"
+                      width="24"
+                      height="24"
+                      focusable="false"
+                    >
+                      <path d="M3 3v15a3 3 0 003 3h9v-6h6V3zm9 8H6v-1h6zm6-3H6V7h12zm-2 8h5l-5 5z"></path>
+                    </svg>
+                  </li-icon>
+                </button>
+
+                <button className="buttonPost d-flex align-items-center">
+                  <li-icon
+                    aria-hidden="true"
+                    type="overflow-web-ios"
+                    class="artdeco-button__icon "
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      data-supported-dps="24x24"
+                      fill="currentColor"
+                      class="mercado-match gray"
+                      width="24"
+                      height="24"
+                      focusable="false"
+                    >
+                      <path d="M14 12a2 2 0 11-2-2 2 2 0 012 2zM4 10a2 2 0 102 2 2 2 0 00-2-2zm16 0a2 2 0 102 2 2 2 0 00-2-2z"></path>
+                    </svg>
+                  </li-icon>
+                </button>
+              </div>
+              <div className="secondContainerPostIcons">
+                <button className="buttonPost">
+                  <li-icon
+                    aria-hidden="true"
+                    type="comment"
+                    class="share-state-change-button__icon"
+                    size="small"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      data-supported-dps="16x16"
+                      fill="currentColor"
+                      class="mercado-match gray"
+                      width="16"
+                      height="16"
+                      focusable="false"
+                    >
+                      <path d="M5 8h5v1H5zm11-.5v.08a6 6 0 01-2.75 5L8 16v-3H5.5A5.51 5.51 0 010 7.5 5.62 5.62 0 015.74 2h4.76A5.5 5.5 0 0116 7.5zm-2 0A3.5 3.5 0 0010.5 4H5.74A3.62 3.62 0 002 7.5 3.53 3.53 0 005.5 11H10v1.33l2.17-1.39A4 4 0 0014 7.58zM5 7h6V6H5z"></path>
+                    </svg>
+                  </li-icon>
+                  <span className="ms-2 gray">Tutti</span>
+                </button>
+              </div>
+              <div>
+                <button className="buttonPost me-1">
+                  <li-icon
+                    aria-hidden="true"
+                    type="clock"
+                    class="artdeco-button__icon"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      data-supported-dps="24x24"
+                      fill="currentColor"
+                      class="mercado-match gray"
+                      width="24"
+                      height="24"
+                      focusable="false"
+                    >
+                      <g>
+                        <path d="M2 12A10 10 0 1012 2 10 10 0 002 12zm2 0a8 8 0 118 8 8 8 0 01-8-8z"></path>
+                        <path d="M15.1 12.63L13 11.42V7a1 1 0 00-2 0v5a1 1 0 00.51.85l2.59 1.52a1 1 0 101-1.74z"></path>
+                      </g>
+                    </svg>
+                  </li-icon>
+                </button>
+              </div>
+              <div>
+              <button type="button" class="rounded-pill px-3 py-1 btn btn-primary me-2" disabled={comment?false:true} onClick={()=>{
+                commentToSend.text=comment;
+                handleClose()
+                setComment(null)
+                sendComment()
+
+              }}>Pubblica</button>
+              </div>
+            </div>
           </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
         </Modal>
-        <div className="PostCreationIconsContainer my-3">
+
+        <div className="my-3 d-flex justify-content-between align-items-center">
           <button className="postButtonContainer">
             <li-icon
               aria-hidden="true"
