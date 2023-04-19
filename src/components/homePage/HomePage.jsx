ec3c72b34ef4cd62c795a46069382f6f3507edae
VinceDV <122369775+VinceDV@users.noreply.github.com>
2023-04-19 18:12:29 +0200

    com

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -1,18 +1,24 @@
-import { Col, Container, Row } from "react-bootstrap"
-import Annuncio from "./ColonnaDestra/Annuncio"
-import LinkedinNotizie from "./ColonnaDestra/LinkedinNotizie"
-import { FooterHome } from "./FooterHome/FooterHome"
-import Messaggistica from "./ColonnaDestra/Messaggistica"
-const HomePage = () => {
-    return (
-        <Container className="pageContainer">
-            <Row>
-                <Col xs={12} md={2}></Col>
-                <Col xs={12} md={7}></Col>
-                <Col xs={12} md={3}><LinkedinNotizie/><Annuncio/><Messaggistica /><FooterHome></FooterHome></Col>
-            </Row>
-        </Container>
-    )
-}
+import { Col, Container, Row } from "react-bootstrap";
+import Annuncio from "./ColonnaDestra/Annuncio";
+import LinkedinNotizie from "./ColonnaDestra/LinkedinNotizie";
+import { FooterHome } from "./FooterHome/FooterHome";
+import Messaggistica from "./ColonnaDestra/Messaggistica";
 
-export default HomePage
+const HomePage = () => {
+  return (
+    <Container className="pageContainer">
+      <Row>
+        <Col xs={12} md={2}></Col>
+        <Col xs={12} md={7}></Col>
+        <Col xs={12} md={3}>
+          <LinkedinNotizie />
+          <Annuncio />
+          <FooterHome></FooterHome>
+          <Messaggistica />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default HomePage;
