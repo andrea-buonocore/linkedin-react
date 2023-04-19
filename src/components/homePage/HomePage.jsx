daab8d0c0ea5805f84564363a4f3cb51bcd25ea4
VinceDV <122369775+VinceDV@users.noreply.github.com>
2023-04-19 12:53:09 +0200

    com

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import { Col, Container, Row } from "react-bootstrap"
+import Annuncio from "./ColonnaDestra/Annuncio"
+import LinkedinNotizie from "./ColonnaDestra/LinkedinNotizie"
 
 const HomePage = () => {
     return (
-        <Container>
+        <Container className="pageContainer">
             <Row>
                 <Col xs={12} md={2}></Col>
                 <Col xs={12} md={7}></Col>
-                <Col xs={12} md={3}></Col>
+                <Col xs={12} md={3}><Annuncio/><LinkedinNotizie/></Col>
             </Row>
         </Container>
     )
