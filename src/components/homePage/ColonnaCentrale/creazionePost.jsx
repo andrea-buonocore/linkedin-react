5030e41bf64b46c9db21208efbdced71e09bf4c5
gretel-hans <adjeihansel@icloud.com>
2023-04-19 16:43:11 +0200

    .

--- src/components/homePage/ColonnaCentrale/creazionePost.jsx
+++ src/components/homePage/ColonnaCentrale/creazionePost.jsx
@@ -98,7 +98,34 @@ const CreazionePost = () => {
                 className="mb-3"
                 controlId="exampleForm.ControlTextarea1"
               >
-                <Form.Control as="textarea" rows={3} placeholder='Di cosa vorresti parlare?' className="textPostArea"/>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  placeholder="Di cosa vorresti parlare?"
+                  className="textPostArea"
+                />
+              </Form.Group>
+              <Form.Group>
+                <div>
+                  <li-icon
+                    aria-hidden="true"
+                    type="emoji-face-icon"
+                    class="artdeco-button__icon"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      data-supported-dps="24x24"
+                      fill="currentColor"
+                      class="mercado-match"
+                      width="24"
+                      height="24"
+                      focusable="false"
+                    >
+                      <path d="M8 10.5A1.5 1.5 0 119.5 12 1.5 1.5 0 018 10.5zm6.5 1.5a1.5 1.5 0 10-1.5-1.5 1.5 1.5 0 001.5 1.5zm7.5 0A10 10 0 1112 2a10 10 0 0110 10zm-2 0a8 8 0 10-8 8 8 8 0 008-8zm-8 4a6 6 0 01-4.24-1.76l-.71.76a7 7 0 009.89 0l-.71-.71A6 6 0 0112 16z"></path>
+                    </svg>
+                  </li-icon>
+                </div>
               </Form.Group>
             </Form>
           </Modal.Body>
