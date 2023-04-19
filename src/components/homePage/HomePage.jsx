1324127a73a13e0864bc9c0286739bcd198789a5
gretel-hans <adjeihansel@icloud.com>
2023-04-19 15:37:37 +0200

    .

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -1,11 +1,17 @@
 import { Col, Container, Row } from "react-bootstrap"
+import CreazionePost from "./ColonnaCentrale/creazionePost"
 
 const HomePage = () => {
     return (
-        <Container>
+        <Container className="pageContainer">
             <Row>
                 <Col xs={12} md={2}></Col>
-                <Col xs={12} md={7}></Col>
+                <Col xs={12} md={7}>
+                    <Row xs={1}>
+                        <Col><CreazionePost /> </Col>
+                    
+                    </Row>
+                </Col>
                 <Col xs={12} md={3}></Col>
             </Row>
         </Container>
