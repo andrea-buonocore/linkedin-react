c3726f490068c064771e3b05074915642741c6cb
gretel-hans <adjeihansel@icloud.com>
2023-04-21 00:29:42 +0200

    .

--- src/components/profilePage/profileSubComponents/Esperienza.jsx
+++ src/components/profilePage/profileSubComponents/Esperienza.jsx
@@ -11,14 +11,67 @@ const Esperienza = () => {
   const experiences = useSelector((state) => state.experiences.experiences);
   const params = useParams();
   const myInfo = useSelector((state) => state.userInfo.me);
-  const [selectedExperienceId, setSelectedExperienceId] = useState(null)
-  console.log("myInfo", myInfo);
+  const [selectedExperienceId, setSelectedExperienceId] = useState(null);
+  //console.log("myInfo", myInfo);
 
-  console.log("experiences", experiences);
+  const esperienza={
+    role: "",
+    company: "",
+    startDate: "",
+    endDate: null, 	// could be null
+    description: "",
+    area: ""
+}
+
+  //console.log("experiences", experiences);
   //console.log(id)
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [modalExperience, setModalExperience] = useState(false);
+  const closeExperience = () => setModalExperience(false);
+  const showExperience = () => setModalExperience(true);
+
   const formData = new FormData();
+  const formDataExperienceImg =new FormData()
+    let i=0;
+
+  const addExperience =async()=>{
+try {
+    let response=await fetch(`https://striveschool-api.herokuapp.com/api/profile/${myInfo._id}/experiences`,{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDNjZjI1YjE4NmE4NzAwMTQzODY3YWUiLCJpYXQiOjE2ODE3MTU4MDMsImV4cCI6MTY4MjkyNTQwM30.QtMkPVJHJwbJXrJQxCZi3t_c8ImEL7Pi8UKRK-l88Tk",
+        },
+        body: JSON.stringify(esperienza)
+      });
+      if(response.ok){
+        const infoExperience= await response.json()
+        const experienceId=infoExperience._id;
+        if(formDataExperienceImg.has('experience')){
+            let responseImg= await fetch(`https://striveschool-api.herokuapp.com/api/profile/${myInfo._id}/experiences/${experienceId}/picture`,{
+                method: "POST",
+                headers: {
+                  Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDNjZjI1YjE4NmE4NzAwMTQzODY3YWUiLCJpYXQiOjE2ODE3MTU4MDMsImV4cCI6MTY4MjkyNTQwM30.QtMkPVJHJwbJXrJQxCZi3t_c8ImEL7Pi8UKRK-l88Tk",
+                },
+                body: formDataExperienceImg,
+              })
+              if(responseImg.ok){
+                alert("You have added your new experience!");
+                i++;
+              }else{
+                alert("ERROR you haven't added your new experience...");
+              }
+        }
+      }
+} catch (error) {
+    console.log('ERROR',error)
+}
+  }
+
 
   const changeExperiencePic = async (id) => {
     console.log("id", id);
@@ -43,6 +96,8 @@ const Esperienza = () => {
       console.log(err);
     }
   };
+
+
 
   const getExperiences = async () => {
     try {
@@ -72,9 +127,9 @@ const Esperienza = () => {
 
   useEffect(() => {
     getExperiences();
-    console.log(params);
+    //console.log(params);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [myInfo]);
+  }, [myInfo,i]);
 
   return (
     <>
@@ -83,10 +138,72 @@ const Esperienza = () => {
           <div className="d-flex justify-content-between">
             <Card.Title>Esperienza</Card.Title>
             <div>
-              <i className="bi bi-plus-lg px-3"></i>
-              <i className="bi bi-pencil"></i>
+              <i
+                type="button"
+                className="bi bi-plus-lg px-3"
+                onClick={showExperience}
+              ></i>
+              <i type="button" className="bi bi-pencil"></i>
             </div>
           </div>
+
+          <Modal show={modalExperience} onHide={closeExperience}>
+            <Modal.Header closeButton>
+              <Modal.Title>Aggiungi nuova esperienza</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label className="mb-1">Ruolo ricoperto</Form.Label>
+                  <Form.Control type="text" placeholder="Inserisci il ruolo ricoperto..." onChange={(e)=>{esperienza.role=e.target.value}}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label className="mb-1">Nome azienda</Form.Label>
+                  <Form.Control type="text" placeholder="Inserisci il nome dell'azienda..." onChange={(e)=>{esperienza.company=e.target.value}}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label className="mb-1">Descrizione</Form.Label>
+                  <Form.Control type="text" placeholder="Inserisci la descrizione del lavoro svolto..." onChange={(e)=>{esperienza.description=e.target.value}}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label className="mb-1">Località</Form.Label>
+                  <Form.Control type="text" placeholder="Inserisci la località..." onChange={(e)=>{esperienza.area=e.target.value}}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label className="mb-1">Data di inizio</Form.Label>
+                  <Form.Control type="date" onChange={(e)=>{esperienza.startDate=e.target.value}}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label className="mb-1">Data di fine</Form.Label>
+                  <Form.Control type="date" onChange={(e)=>{esperienza.endDate=e.target.value}}/>
+                </Form.Group>
+                <Form.Group controlId="formFile" className="mb-3">
+                <Form.Label className="mb-1">Allega un'immagine</Form.Label>
+                <Form.Control
+                  type="file"
+                  onChange={(e) => {
+                    const file = e.target.files[0];
+                    formDataExperienceImg.append("experience", file);
+                  }}
+                />
+              </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={closeExperience}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={()=>{
+                addExperience()
+                closeExperience()
+                console.log(esperienza)
+                }}>
+                Aggiungi esperienza
+              </Button>
+            </Modal.Footer>
+          </Modal>
+
+
           {experiences.map((experience, index) => {
             let startDate = new Date(experience.startDate);
             let startYear = startDate.getFullYear();
@@ -107,7 +224,7 @@ const Esperienza = () => {
                         }
                         onClick={() => {
                           handleShow();
-                          setSelectedExperienceId(experience._id)
+                          setSelectedExperienceId(experience._id);
                           console.log("img selezionata", experience._id);
                         }}
                       />
