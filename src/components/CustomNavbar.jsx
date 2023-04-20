413cf9f258d3cde4970630cf6c5108d5bb63fae2
gretel-hans <adjeihansel@icloud.com>
2023-04-20 17:44:41 +0200

    .

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -50,10 +50,8 @@ const CustomNavbar = () => {
               </a>
             </li>
             <li>
-              <a
-                href="#"
-                className="d-flex flex-column align-items-center justify-content-end"
-              >
+
+                <Link to='/' className="d-flex flex-column align-items-center justify-content-end"> 
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="25"
@@ -65,7 +63,8 @@ const CustomNavbar = () => {
                   <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z" />
                 </svg>{" "}
                 <p className="d-none d-lg-inline m-0">Home</p>
-              </a>
+                </Link>
+              
             </li>
             <li>
               <a href="#" className="d-flex flex-column align-items-center">
