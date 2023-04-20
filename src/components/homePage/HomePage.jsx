439d9f0dac8ae9b54b5f8e47c9fd4a589e153b42
StefaniaFracasso <122379841+StefaniaFracasso@users.noreply.github.com>
2023-04-20 12:17:40 +0200

    aggiunto componente ScrollToTopButton

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -5,12 +5,13 @@ import LinkedinNotizie from "./ColonnaDestra/LinkedinNotizie";
 import { FooterHome } from "./FooterHome/FooterHome";
 import Messaggistica from "./ColonnaDestra/Messaggistica";
 
-import HomeProfile from "./ColonnaSinistra/HomeProfile"
+import HomeProfile from "./ColonnaSinistra/HomeProfile";
 import { useDispatch, useSelector } from "react-redux";
 import Spinner from "react-bootstrap/Spinner";
 import Post from "./ColonnaCentrale/Post";
 import CreazionePost from "./ColonnaCentrale/creazionePost";
 import Scopri from "./ColonnaSinistra/Scopri";
+import ScrollToTopButton from "./ColonnaDestra/ScrollToTopButton";
 
 const HomePage = () => {
   const dispatch = useDispatch();
@@ -20,33 +21,33 @@ const HomePage = () => {
   //const myInfo=useSelector(state=>state.myInfo)
   const [isLoading, setIsLoading] = useState(true);
 
-    const getPosts = async () => {
-        try {
-            let response = await fetch('https://striveschool-api.herokuapp.com/api/posts/', {
-                method: "GET",
-                headers: {
-                    Authorization:
-                        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDNjZjI1YjE4NmE4NzAwMTQzODY3YWUiLCJpYXQiOjE2ODE3MTU4MDMsImV4cCI6MTY4MjkyNTQwM30.QtMkPVJHJwbJXrJQxCZi3t_c8ImEL7Pi8UKRK-l88Tk",
-                },
-            });
-            if (response.ok) {
-                let post = await response.json();
-                console.log('tutti i post', post);
-                let fiftyPost = post.reverse().slice(0, 50);
-                dispatch({
-                    type: 'SAVE_POST',
-                    payload: fiftyPost
-                })
-                setIsLoading(false);
-
-            }
-            else {
-                return new Error('Errore nella fetch:', response.status);
-            }
+  const getPosts = async () => {
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/posts/",
+        {
+          method: "GET",
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDNjZjI1YjE4NmE4NzAwMTQzODY3YWUiLCJpYXQiOjE2ODE3MTU4MDMsImV4cCI6MTY4MjkyNTQwM30.QtMkPVJHJwbJXrJQxCZi3t_c8ImEL7Pi8UKRK-l88Tk",
+          },
         }
-        catch (err) {
-            console.error(err);
-        }
+      );
+      if (response.ok) {
+        let post = await response.json();
+        console.log("tutti i post", post);
+        let fiftyPost = post.reverse().slice(0, 50);
+        dispatch({
+          type: "SAVE_POST",
+          payload: fiftyPost,
+        });
+        setIsLoading(false);
+      } else {
+        return new Error("Errore nella fetch:", response.status);
+      }
+    } catch (err) {
+      console.error(err);
+    }
 
     try {
       let response = await fetch(
@@ -72,45 +73,40 @@ const HomePage = () => {
   };
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() =>{
-    getPosts()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  } ,[counter]);
+  useEffect(() => {
+    getPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [counter]);
 
   return (
     <Container className="pageContainer">
       <Row>
         <Col xs={12} md={3}>
-                    <HomeProfile/>
-                    <Scopri/>
-
-                </Col>
+          <HomeProfile />
+          <Scopri />
+        </Col>
         <Col xs={12} md={6}>
-                    <Row xs={1}>
-                        <Col><CreazionePost /> </Col>
-                    
-                    </Row>
-                
-                    {
-                        isLoading && (
-                            <div className="text-center">
-                            <Spinner animation="border" variant="primary"/>
-                            </div>
-                        )
-                    }
-                    {
-                        postRedux.map((post, index) => {
-                            return (
-                                <Post post={post} key={index} />
-                            )
-                        })
-                    }
-                </Col>
+          <Row xs={1}>
+            <Col>
+              <CreazionePost />{" "}
+            </Col>
+          </Row>
+
+          {isLoading && (
+            <div className="text-center">
+              <Spinner animation="border" variant="primary" />
+            </div>
+          )}
+          {postRedux.map((post, index) => {
+            return <Post post={post} key={index} />;
+          })}
+        </Col>
         <Col xs={12} md={3}>
           <LinkedinNotizie />
           <Annuncio />
           <FooterHome></FooterHome>
           <Messaggistica />
+          <ScrollToTopButton/>
         </Col>
       </Row>
     </Container>
